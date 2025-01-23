4b5ced71665a737a6f2cf909aa34467b33391a62
Rajesh050691 <rajeshbe5691@gmail.com>
2025-01-23 17:36:33 +0530

    Project

---
+++ src/org/dev/datatypes/user.java
@@ -0,0 +1,39 @@
+package org.dev.datatypes;
+
+public class user {
+	public static void main(String[] args) {
+		byte age =22;
+		System.out.println("age :" + age);
+		short atmpin = 1312;
+		System.out.println("atmpin :" + atmpin);
+		int pincode =600075;
+		System.out.println("pincode :" + pincode);
+		long phonenumber =9789041642l;
+		System.out.println("phonenumber :" + phonenumber);
+		float salary = 30000.00f;
+		System.out.println("salary :" + salary);
+		double interestRate =4444.4444;
+		System.out.println("interestrate :" + interestRate);
+		char gender = 'm';
+		System.out.println("gender :" + gender);
+		String name = "rajesh";
+		System.out.println("name :" + name);
+		boolean result = true;
+		System.out.println("result :" + result);
+		String date = "05-06-1991";
+		System.out.println("date :" + date);
+		
+		System.out.println(20 + 30);
+		System.out.println("addition:" +20 +30);
+		System.out.println(20 + 30 + "addition");
+		System.out.println(20 + "addition" +30);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
